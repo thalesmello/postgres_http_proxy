b803b6acba35560c0cf9037384dc2f259d8140a0
Thales Mello <thalesmello@gmail.com>
2018-09-11 04:56:01 -0300

    Fix problem with default setting initialization script

--- docker-entrypoint-initdb.d/setup.sh
+++ docker-entrypoint-initdb.d/setup.sh
@@ -3,5 +3,5 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION http;
-    ALTER DATABASE SET http.timeout_ms = $HTTP_TIMEOUT_MS;
+    ALTER DATABASE $POSTGRES_DB SET http.timeout_msec = $HTTP_TIMEOUT_MS;
 EOSQL
